c8828145a8fd8c993bb5a8ae893b40a44f390e70
Muhammad Riza <muhammadriza@gmail.com>
2014-11-07 10:07:44 +0700

    tambahan komentar

--- conf.php
+++ conf.php
@@ -1,19 +1,19 @@
 <?php
-$homedir= "sim";
-$template="default";
-$home_uri="http://".$_SERVER["HTTP_HOST"]."/$homedir/";
-$template_uri=$home_uri."templates/$template/";
-$home=$home_uri."home.php/";
-$dashboard=$home_uri."home.php";
+$homedir= "sim";  //direktori SIM
+$template="default";  //nama template
+$home_uri="http://".$_SERVER["HTTP_HOST"]."/$homedir/";  //url SIM
+$template_uri=$home_uri."templates/$template/";   //url template
+$home=$home_uri."home.php/";  //alamat home.php, router bagi SIM dilengkapi dengan / (slash)
+$dashboard=$home_uri."home.php";  //url dashboard
 
-$servername = "localhost";
-$username = "sim";
-$password = "sim";
-$dbname="sim";
+$servername = "localhost";  //alamat server database
+$username = "sim";  //username database
+$password = "sim";  //password database
+$dbname="sim";  //database
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);   //koneksi ke database
 
 if ($conn->connect_error) {
-	die("Koneksi ke basisdata gagal: " . $conn->connect_error);
+	die("Koneksi ke basisdata gagal: " . $conn->connect_error);   //tampilkan pesan kesalahan jika gagal koneksi ke database
 }
 ?>
