2cb12038458a12d899411b727381d93e48c81471
Muhammad Riza <muhammadriza@gmail.com>
2014-11-08 21:56:45 +0700

    End of line type changed to UNIX format

--- help/about.php
+++ help/about.php
@@ -1,5 +1,4 @@
 <div id="about">
-<p>SIM versi 0.0.11 build 20141105</p>
 <h3>Tentang SIM</h3>
 <p>Small Indsutry Manager atau disingkat SIM adalah sebuah aplikasi sederhana berbasis web yang dapat digunakan untuk melakukan pengelolaan keuangan dan inventory industri kecil. SIM dirancang dengan sudut pandang aliran kas (masuk dan keluar) yang sederhana, sehingga dapat digunakan dengan mudah, tanpa harus belajar teori-teori akuntansi terlebih dahulu.</p>
 <p>Antarmuka SIM dibagun dengan teknologi web HTML5 dan CSS3, oleh karena itu pastikan peramban yang digunakan sudah mendukung teknologi web HTML5 dan CSS3 seperti Mozilla Firefox, Google Chrome dan Opera. Peramban Microsoft Internet Explorer dan Apple Safari juga dapat digunakan walaupun beberapa bagian aplikasi tidak tampil dengan semestinya. Teknologi pendukung pengolahan SIM adalah PHP dan teknologi basisdata MySQL atau MariaDB. Jika anda ingin memasang sendiri SIM pada komputer anda atau peladen khusus, harap baca panduan teknisi.</p>
